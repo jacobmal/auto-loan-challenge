1d878c2dfaf18dacffbaef476853218d2ed988b0
Jake Allen <jacobmal@umich.edu>
2021-12-04 17:30:39 -0600

    Remove unnecessary boilerplate and add bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Using just bootstrap styles to save time
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
