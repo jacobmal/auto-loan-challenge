ad8de527453054aedd5d3c0cac156734d2168ca4
Jake Allen <jacobmal@umich.edu>
2021-12-05 19:40:05 -0600

    Create the Create Account Form

--- src/CreateAccount.tsx
+++ src/CreateAccount.tsx
@@ -1,12 +1,29 @@
 import { FunctionComponent } from "react";
+import { Container, Row, Form, Button } from "react-bootstrap";
 
 interface CreateAccountProps {
     
 }
  
 const CreateAccount: FunctionComponent<CreateAccountProps> = () => {
-    return (  
-        <div>Create Account Page</div>
+    // TODO:    Also remove duplicate code here
+    return (
+        <Container>
+            <Row className="mt-4">
+                <h2>Create Account</h2>
+                <Form>
+                    <Form.Group className="mb-3" controlId="autoMake">
+                        <Form.Label>Email Address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter Email" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="autoMake">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Enter Password" />
+                    </Form.Group>
+                    <Button variant="primary">Submit</Button>
+                </Form>
+            </Row>
+        </Container>  
     );
 }
  
